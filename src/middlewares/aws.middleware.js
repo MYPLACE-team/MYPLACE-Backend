ebc500398c9dfff3c194e9ee6b7c20b97d76e1f8
Junstory <lastcom1@gmail.com>
2024-01-26 17:05:50 +0900

    #18 Feat: S3 업로드 API

---
+++ src/middlewares/aws.middleware.js
@@ -0,0 +1,34 @@
+import AWS from 'aws-sdk'
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config()
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_REGION,
+})
+
+const s3 = new AWS.S3()
+
+const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp']
+
+export const imageUploader = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME, // 생성한 버킷 이름을 적어주세요.
+    key: (req, file, callback) => {
+      const uploadDirectory = req.query.directory ?? 'profile' // 업로드할 디렉토리를 설정하기 위해 넣어둔 코드로, 없어도 무관합니다.
+      const extension = path.extname(file.originalname)
+      console.log(extension)
+      if (!allowedExtensions.includes(extension)) {
+        // extension 확인을 위한 코드로, 없어도 무관합니다.
+        return callback(new Error('wrong extension'))
+      }
+      callback(null, `${uploadDirectory}/profile_${file.originalname}`)
+    },
+    acl: 'public-read-write',
+  }),
+})
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import { authLogin, authJWT } from '../controllers/auth.controller'
+import { imageUploader } from '../middlewares/aws.middleware'
 
 export const authRouter = express.Router({ mergeParams: true })
 
 authRouter.post('/login', authLogin)
 authRouter.get('/login', authJWT) //임시
+authRouter.post('/upload', imageUploader.single('image'), (req, res) => {
+  try {
+    res.send('success!')
+  } catch (err) {
+    res.send(err)
+  }
+}) //임시
